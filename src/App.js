3aabfb1289ff6a40b6dbe6c346b5b016a6f4df96
naeemujeeb <leguy789@gmail.com>
2023-09-30 20:30:17 +0100

    Added images to food order and created a multi view

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import './App.css';
-import React, { useState } from "react";
+import "./App.css";
+import Foods from "./Food"
+import React, {Fragment, useState } from "react";
+
 
 const App = () => {
-  const [menuItems, setMenuItems] = useState([
+    const [isChooseFoodPage, setIsChooseFoodPage] = useState(false);
+    const [menuItems, setMenuItems] = useState([
     {
       id: 1,
       name: "Chicken Burger",
       quantity: 40,
       desc: "Fried chicken burger - lettuce, tomato, cheese and mayonnaise",
       price: "24",
-      image: "cb.jpg",
+      image: "chicke.jpg",
 }, {
       id: 2,
       name: "Veg Burger",
       quantity: 30,
       desc: "Plant-based burger - lettuce, tomato, vegan cheese and mayonnaise",
       price: "22",
-      image: "vb.jpg",
+      image: "cb.jpg",
 }, {
       id: 3,
       name: "Chips",
       quantity: 50,
       desc: "Potato chips fried to perfection",
       price: "7",
-      image: "chips.jpg",
+      image: "pot.jpg",
 }, {
       id: 4,
       name: "Ice Cream",
       quantity: 30,
-      desc: "Ice cream - Vanilla ice cream double scoop",
+    desc: "Ice cream - Vanilla ice cream double scoop",
       price: "4",
-      image: "ic.jpg",
-},
-
-  ])
-}
-
-
+      image: "sand.jpg",
+}, ]);
+ return (
+    <div className="App">
+      <button className="toggleButton" onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}>
+        {isChooseFoodPage ? "Availability Check" : "Order Food"}
+      </button>
+      <h3 className="title">Just Food Online Shop</h3>
+      {!isChooseFoodPage && ( 
+      <Fragment>
+      <h4 className="subTitle">Menu Availability</h4>
+      <ul className="ulApp">
+        {menuItems.map((item) => {
+          return (
+            <li key={item.id} className="liApp">
+              {item.name} - {item.quantity}
+</li> );
+})} </ul></Fragment>)}
+     {isChooseFoodPage && <Foods foodItems={menuItems}></Foods>}
+</div> );
+};
 
 export default App;
